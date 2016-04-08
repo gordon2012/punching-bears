f5f4e083cebd05176fac11df003bf2dd89d5b407
Gordon Doskas <gordon2012@users.noreply.github.com>
2016-04-07 20:47:08 -0700

    Spawning Our First Player

--- PunchingBearsUnityProject/Assets/Network.cs
+++ PunchingBearsUnityProject/Assets/Network.cs
@@ -4,13 +4,17 @@ using SocketIO;
 
 public class Network : MonoBehaviour {
 
+    public GameObject playerPrefab;
+
     static SocketIOComponent socket;
 
 	// Use this for initialization
 	void Start () {
         socket = GetComponent<SocketIOComponent>();
         socket.On("open", OnConnected);
+        socket.On("spawn", OnSpawned);
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,5 +25,11 @@ public class Network : MonoBehaviour {
     {
         Debug.Log("connected");
         socket.Emit("move");
+    }
+
+    private void OnSpawned(SocketIOEvent e)
+    {
+        Debug.Log("spawned");
+        Instantiate(playerPrefab);
     }
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ var io = require('socket.io')(process.env.PORT || 3000);
 console.log('server started');
 
 io.on('connection', function(socket) {
-	console.log('client connected');
+	console.log('client connected, broadcasting spawn');
+
+	socket.broadcast.emit('spawn');
 
 
 	socket.on('move', function(data) {
